c34bceb6dba4dbf0d99f1c89152a73f33d572183
Matteo Badini <matteo.badini95@gmail.com>
2019-04-22 13:58:14 +0200

    added rollup basic config and yarn

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,21 @@
 import { eslint } from 'rollup-plugin-eslint'
 import { terser } from 'rollup-plugin-terser'
 
-const devPlugins = [eslint()]
-const prdPlugins = [eslint(), terser()]
+const plugins =
+	process.env.BUILD !== 'development'
+		? [eslint(), terser({ warnings: 'verbose' })]
+		: [eslint()]
+
+const outputFile =
+	process.env.BUILD !== 'development'
+		? 'dist/focus-within.min.js'
+		: 'dist/focus-within.js'
 
 export default {
 	input: './src/focus-within.js',
 	output: {
-		file: 'dist/focus-within.min.js',
+		file: outputFile,
 		format: 'iife'
 	},
-	plugins: process.env.BUILD === 'development' ? devPlugins : prdPlugins
+	plugins: plugins
 }
