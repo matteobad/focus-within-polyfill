c15a209413313dd13e5342330ec7cd8de5b82802
Matteo Badini <matteo.badini95@gmail.com>
2019-06-02 21:07:13 +0200

    refactor: switch to google eslint configuration

--- src/focus-within.js
+++ src/focus-within.js
@@ -1,139 +1,141 @@
 /**
  * :focus-within Polyfill
  *
- * @param {String} selector
- * @returns {Boolean}
+ * @param {string|null} selector
  */
-function polyfill (selector) {
-	var attrName, attrValue, activeElement, lastActiveElement
+function polyfill(selector) {
+  let attrName; let attrValue; let activeElement; let lastActiveElement;
 
-	// Variables initialization
-	selector = selector || '[focus-within]'
-	selector = (selector.indexOf('.') !== 0)
-		? (attrName = attrValue = selector.replace(/[[\]']+/g, ''))
-		: (attrName = 'class', attrValue = selector.replace('.', ''))
+  // Variables initialization
+  selector = selector || '[focus-within]';
+  selector = (selector.indexOf('.') !== 0)
+    ? (attrName = attrValue = selector.replace(/[[\]']+/g, ''))
+    : (attrName = 'class', attrValue = selector.replace('.', ''));
 
-	/**
-	 * Find and return the current Element with focus.
-	 * This will loop through shadow DOM.
-	 *
-	 * @returns {Element}
-	 */
-	function findActiveElement () {
-		var activeEl = document.activeElement
-		while (activeEl && activeEl.shadowRoot && activeEl.shadowRoot.activeElement) {
-			activeEl = activeEl.shadowRoot.activeElement
-		}
-		return activeEl
-	}
+  /**
+   * Find and return the current Element with focus.
+   * This will loop through shadow DOM.
+   *
+   * @return {Element}
+   */
+  function findActiveElement() {
+    let el = document.activeElement;
+    while (el && el.shadowRoot && el.shadowRoot.activeElement) {
+      el = el.shadowRoot.activeElement;
+    }
+    return el;
+  }
 
-	/**
-	 * Create and return the event path from root to element.
-	 * The computed path includes element inside a shadowRoot.
-	 *
-	 * @param {Element} el
-	 * @returns {Array}
-	 */
-	function computeEventPath (el) {
-		var path = []
-		while (el && (el.nodeType === 1 || el.nodeType === 11)) {
-			if (el.nodeType !== 11) {
-				path.push(el)
-				el = el.parentNode
-			} else {
-				el = el.host
-			}
-		}
-		return path
-	}
+  /**
+   * Create and return the event path from root to element.
+   * The computed path includes element inside a shadowRoot.
+   *
+   * @param {Element} el
+   * @return {Array}
+   */
+  function computeEventPath(el) {
+    const path = [];
+    while (el && (el.nodeType === 1 || el.nodeType === 11)) {
+      if (el.nodeType !== 11) {
+        path.push(el);
+        el = el.parentNode;
+      } else {
+        el = el.host;
+      }
+    }
+    return path;
+  }
 
-	/**
-	 * Add user defined attribute to element retaining any previously applied attributes.
-	 * Attribute can be the 'class' attribute for compatibility reasons.
-	 *
-	 * @param {String} name
-	 * @param {String} value
-	 */
-	function addAttribute (name, value) {
-		return function (el) {
-			var attributes = el.getAttribute(name) || ''
-			if (attributes.indexOf(value) === -1) {
-				var newAttributes = (attributes + ' ' + value).trim()
-				el.setAttribute(name, newAttributes)
-			}
-		}
-	}
+  /**
+   * Add user defined attribute to element retaining any previously
+   * applied attributes. Attribute can be the 'class' attribute for
+   * compatibility reasons.
+   *
+   * @param {string} name
+   * @param {string} value
+   * @return {function} callback
+   */
+  function addAttribute(name, value) {
+    return function(el) {
+      const attributes = el.getAttribute(name) || '';
+      if (attributes.indexOf(value) === -1) {
+        const newAttributes = (attributes + ' ' + value).trim();
+        el.setAttribute(name, newAttributes);
+      }
+    };
+  }
 
-	/**
-	 * Remove user defined attribute value or entire attribute if last one.
-	 * Attribute can be the 'class' attribute for compatibility reasons.
-	 *
-	 * @param {String} name
-	 * @param {String} value
-	 */
-	function removeAttribute (name, value) {
-		return function (el) {
-			var attributes = el.getAttribute(name) || ''
-			if (attributes.indexOf(value) !== -1) {
-				var newAttributes = attributes.replace(value, '').trim()
-				if (newAttributes === '') el.removeAttribute(name)
-				else el.setAttribute(name, newAttributes)
-			}
-		}
-	}
+  /**
+   * Remove user defined attribute value or entire attribute if last one.
+   * Attribute can be the 'class' attribute for compatibility reasons.
+   *
+   * @param {string} name
+   * @param {string} value
+   * @return {function} callback
+   */
+  function removeAttribute(name, value) {
+    return function(el) {
+      const attributes = el.getAttribute(name) || '';
+      if (attributes.indexOf(value) !== -1) {
+        const newAttributes = attributes.replace(value, '').trim();
+        if (newAttributes === '') el.removeAttribute(name);
+        else el.setAttribute(name, newAttributes);
+      }
+    };
+  }
 
-	/**
-	 * Use rAF to remove and apply custom attribute on FocusEvent.
-	 *
-	 * @param {FocusEvent} e
-	 */
-	function handler (e) {
-		var running
-		activeElement = findActiveElement()
+  /**
+   * Attach event listerns to initiate polyfill
+   */
+  function attachListeners() {
+    const handler = function(e) {
+      let running;
+      activeElement = findActiveElement();
 
-		function action () {
-			running = false
+      /**
+       * Request animation frame callback.
+       * Remove previously applied attributes.
+       * Add new attributes.
+       */
+      function action() {
+        running = false;
 
-			Array.prototype.slice
-				.call(computeEventPath(lastActiveElement))
-				.forEach(removeAttribute(attrName, attrValue))
+        Array.prototype.slice
+            .call(computeEventPath(lastActiveElement))
+            .forEach(removeAttribute(attrName, attrValue));
 
-			lastActiveElement = activeElement
-			if (e.type !== 'focus' || !activeElement) return
+        lastActiveElement = activeElement;
+        if (e.type !== 'focus' || !activeElement) return;
 
-			Array.prototype.slice
-				.call(computeEventPath(activeElement))
-				.forEach(addAttribute(attrName, attrValue))
-		}
+        Array.prototype.slice
+            .call(computeEventPath(activeElement))
+            .forEach(addAttribute(attrName, attrValue));
+      }
 
-		if (!running) {
-			window.requestAnimationFrame(action)
-			running = true
-		}
-	}
+      if (!running) {
+        window.requestAnimationFrame(action);
+        running = true;
+      }
+    };
 
-	/**
-	 * Attach event listerns to initiate polyfill
-	 */
-	function attachListeners () {
-		document.addEventListener('focus', handler, true)
-		document.addEventListener('blur', handler, true)
-	}
+    document.addEventListener('focus', handler, true);
+    document.addEventListener('blur', handler, true);
+  }
 
-	try {
-		document.querySelector(':focus-within')
-		if (window.forceFocusWithinPolyfill) {
-			attachListeners()
-		}
-	} catch (error) {
-		attachListeners()
-	}
+  try {
+    document.querySelector(':focus-within');
+    if (window.forceFocusWithinPolyfill) {
+      attachListeners();
+    }
+  } catch (error) {
+    attachListeners();
+  }
 }
 
 if (typeof exports === 'object' && typeof module !== 'undefined') {
-	module.exports = {
-		polyfill: polyfill
-	}
+  module.exports = {
+    polyfill: polyfill,
+  };
 } else {
-	polyfill()
+  polyfill(null);
 }
--- test/buttons.spec.js
+++ test/buttons.spec.js
@@ -1,21 +1,24 @@
-const fs = require('fs')
-const path = require('path')
-const html = fs.readFileSync(path.resolve(__dirname, '../demos/buttons.html'), 'utf8')
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve(
+    __dirname,
+    '../demos/buttons.html'
+), 'utf8');
 
-jest.dontMock('fs')
+jest.dontMock('fs');
 
 beforeEach(() => {
-	document.documentElement.innerHTML = html.toString()
-})
+  document.documentElement.innerHTML = html.toString();
+});
 
 afterEach(() => {
-	// restore the original func after test
-	jest.resetModules()
-})
+  // restore the original func after test
+  jest.resetModules();
+});
 
 describe('button', () => {
-	test('Standard button should exists', () => {
-		const button = document.getElementById('button')
-		expect(button).toBeTruthy()
-	})
-})
+  test('Standard button should exists', () => {
+    const button = document.getElementById('button');
+    expect(button).toBeTruthy();
+  });
+});
